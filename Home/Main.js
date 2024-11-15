83ad7176ab70ccab83c8fa8087faa574bb4adb30
Daryl025 <AroldDaryl.TchoualaMboumanah@edu.sait.ca>
2024-11-15 03:56:36 -0800

    Add dynamic copyright year update to footer
    - Link the JavaScript file to the HTML successfully
    -Retrieve and logged current year and full date using the Date() object
    -Update footer copyright element to ---Display the current year dynamically using the 'innerHTML'

---
+++ Home/Main.js
@@ -0,0 +1,20 @@
+console.log("javascript file is successfuly linked!"); // testing
+
+//getting the current year using js date() object
+const CurrentYear = new Date().getFullYear();
+
+// Displaying current year in console
+console.log(CurrentYear);
+
+//getting the current full date
+const FullDate= new Date().toString();
+
+// display current date
+console.log(FullDate);
+
+// using the get element by id method to find the footers copyrighted element
+const CopyrightElement= document.getElementById("Copyright");
+
+//Update the content with the new year
+CopyrightElement.innerHTML=`&copy; Daryl Tchouala,${CurrentYear}`; // used innerHTML because i used (&copy) which 
+                                                                   // is an HTML entity
