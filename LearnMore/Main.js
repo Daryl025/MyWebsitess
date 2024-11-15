6f72939bccbe620e1138869bd04a1f7d1eea57e1
Daryl025 <AroldDaryl.TchoualaMboumanah@edu.sait.ca>
2024-11-15 09:17:31 -0800

    Create an Ordered List of Even and odd numbers with JavaScript
    -Add ordered list with id 'numbers' in HTML
    -Use javascript for loopp to generate 12 list items
    -Implement the i++ to icrement loop counter more efficiently

--- LearnMore/Main.js
+++ LearnMore/Main.js
@@ -17,3 +17,25 @@ const CopyrightElement= document.getElementById("Copyright");
 
 //Update the content with the new year
 CopyrightElement.innerHTML=`&copy; Daryl Tchouala,${CurrentYear}`; // used innerHTML because i used (&copy) which   is an HTML entity
+
+
+//Getting the list element by ID
+
+const numberlist = document.getElementById('numbers');
+
+//Loop through the numbers 1-12
+for (let i = 1; i <= 12; i++){
+    // new list element 
+    const listItem = document.createElement('li');
+
+      // checking index
+    if (i % 2 == 0) {
+        //if even set text to even
+        listItem.textContent = `Even (${i})`;
+    } else {
+         //if odd set text to odd
+         listItem.textContent = `odd ${i}`;
+    } 
+       // append list item to the orderes list
+      numberlist.appendChild(listItem);
+    }
